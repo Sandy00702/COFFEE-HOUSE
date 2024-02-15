8657c985f6a2624c06a4653de17ea7f1ef51bf47
Sandy00702 <97554183+Sandy00702@users.noreply.github.com>
2024-02-16 02:57:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching UNRAVEL"];
+const statusMessages = ["This is a exclusive bot for coffee house server !"];
 
 
 let currentIndex = 0;
